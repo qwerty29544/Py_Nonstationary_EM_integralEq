e0d38030dc67fc0640836a821c4c474d88824fec
qwerty29544 <qwerty29544@gmail.com>
2021-09-18 08:20:40 +0300

    plate_coeffs

--- src/Plate_4_8.py
+++ src/Plate_4_8.py
@@ -7,44 +7,47 @@ from envir_paths import *
 
 create_base_proj_structure()
 
+N = [16, 24, 32, 40, 48, 56, 64, 72, 80, 100]
+list_of_figures = []
+for n in N:
+    figure = "_".join(["plate", n, n])
+    list_of_figures.append(figure)
 
-plate_12_12 = Figure("d:\\Python\\Py_Nonstationary_EM_integralEq\\figures\\plate_12_12.dat")
-
-
-coeffs_G3 = compute_coeffs.compute_G3_coefficients(frame=plate_12_12.frames[0],
-                                                   collocation=plate_12_12.collocations[0],
-                                                   number_of_frames=plate_12_12.total_frames_in_objects[0],
-                                                   integration_method=integrate_func,
-                                                   max_diameter=plate_12_12.max_diameter[0],
-                                                   n_vertex=4,
-                                                   num_slices=4)
-print("G3")
-compute_coeffs.coeffs_save(coeffs_G3, "d:\\Python\\Py_Nonstationary_EM_integralEq\\coeffs\\plate_12_12\\G3_plate_12_12.txt")
-
-
-coeffs_G1 = compute_coeffs.compute_coeffs(frame=plate_12_12.frames[0],
-                                          collocation=plate_12_12.collocations[0],
-                                          number_of_frames=plate_12_12.total_frames_in_objects[0],
-                                          integration_method=integrate_func,
-                                          integral_function=G1_func,
-                                          max_diameter=plate_12_12.max_diameter[0],
-                                          ndim=3,
-                                          n_vertex=4,
-                                          num_slices=4,
-                                          num_slices_diag=3)
-print("G1")
-compute_coeffs.coeffs_save(coeffs_G1, "d:\\Python\\Py_Nonstationary_EM_integralEq\\coeffs\\plate_12_12\\G1_plate_12_12.txt")
-
-coeffs_G2 = compute_coeffs.compute_coeffs(frame=plate_12_12.frames[0],
-                                          collocation=plate_12_12.collocations[0],
-                                          number_of_frames=plate_12_12.total_frames_in_objects[0],
-                                          integration_method=integrate_func,
-                                          integral_function=G2_func,
-                                          max_diameter=plate_12_12.max_diameter[0],
-                                          ndim=3,
-                                          n_vertex=4,
-                                          num_slices=4,
-                                          num_slices_diag=3)
-print("G2")
-compute_coeffs.coeffs_save(coeffs_G2, "d:\\Python\\Py_Nonstationary_EM_integralEq\\coeffs\\plate_12_12\\G2_plate_12_12.txt")
-
+for fig in list_of_figures:
+    plate = Figure(os.path.join(FIGURES_PATH, fig + ".dat"))
+    coeffs_G1 = compute_coeffs.compute_coeffs(frame=plate.frames[0],
+                                              collocation=plate.collocations[0],
+                                              number_of_frames=plate.total_frames_in_objects[0],
+                                              integration_method=integrate_func,
+                                              integral_function=G1_func,
+                                              max_diameter=plate.max_diameter[0],
+                                              ndim=3,
+                                              n_vertex=4,
+                                              num_slices=4,
+                                              num_slices_diag=3)
+    print("G1 of " + fig)
+    compute_coeffs.coeffs_save(coeffs=coeffs_G1,
+                               filename=os.path.join(COEFFS_PATH, fig, "G1_" + fig + ".txt"))
+    coeffs_G2 = compute_coeffs.compute_coeffs(frame=plate.frames[0],
+                                              collocation=plate.collocations[0],
+                                              number_of_frames=plate.total_frames_in_objects[0],
+                                              integration_method=integrate_func,
+                                              integral_function=G2_func,
+                                              max_diameter=plate.max_diameter[0],
+                                              ndim=3,
+                                              n_vertex=4,
+                                              num_slices=4,
+                                              num_slices_diag=3)
+    compute_coeffs.coeffs_save(coeffs=coeffs_G2,
+                               filename=os.path.join(COEFFS_PATH, fig, "G2_" + fig + ".txt"))
+    print("G2 of " + fig)
+    coeffs_G3 = compute_coeffs.compute_G3_coefficients(frame=plate.frames[0],
+                                                       collocation=plate.collocations[0],
+                                                       number_of_frames=plate.total_frames_in_objects[0],
+                                                       integration_method=integrate_func,
+                                                       max_diameter=plate.max_diameter[0],
+                                                       n_vertex=4,
+                                                       num_slices=4)
+    compute_coeffs.coeffs_save(coeffs=coeffs_G3,
+                               filename=os.path.join(COEFFS_PATH, fig, "G3_" + fig + ".txt"))
+    print("G3 of " + fig)
